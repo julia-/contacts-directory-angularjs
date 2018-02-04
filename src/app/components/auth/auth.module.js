0e8e79ad1b86c8db10e80166b353c57c87406336
julia- <julia.ryanx@gmail.com>
2018-02-04 19:13:51 +1100

    configure firebase

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,18 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function($firebaseRefProvider) {
+    var config = {
+      apiKey: 'AIzaSyBWcDfH9N0K04Z6a0LtPea4fAcw1vya5Dk',
+      authDomain: 'contacts-manager-app-f218b.firebaseapp.com',
+      databaseURL: 'https://contacts-manager-app-f218b.firebaseio.com',
+    }
+
+    $firebaseRefProvider.registerUrl({
+      default: config.databaseURL,
+      contacts: config.databaseURL + '/contacts'
+    })
+
+    firebase.initializeApp(config)
+  })
